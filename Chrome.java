d394308c4d69ead44b77600cc4983d084a7aaa74
Vinodkumar7vk <mailtovinodkumar.3093@gmail.com>
2025-03-02 00:34:54 -0800

    Add files via upload
    Day1 home assignment

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version= 91.0f;
+String developer= "Google";
+boolean isBeta = false;
+int releaseYear=2008;
+char shortcutkey='C';
+
+System.out.println(version + developer + isBeta + releaseYear +shortcutkey);
+
+	}
+
+}
